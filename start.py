7a687f735ce1d1788190d0dcf9011b7c91d4644b
illusivedeveloper <illusivedeveloper@gmail.com>
2020-05-13 01:14:42 +0530

    initial workflow setup

---
+++ start.py
@@ -0,0 +1,16 @@
+class UserInput:
+    def __init__(self, state, province):
+        pass
+
+class Cloudformation(UserInput):
+    def __init__(self):
+        super().__init__()
+
+    def create_lambda_Stack(self):
+        pass
+
+    def deploy_lambda_Stack(self):
+        pass
+
+if __name__ == "__main__":
+    pass
